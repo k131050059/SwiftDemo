9a0216759f87f5a2597a5361efcbd16bc48048f4
ShengJL <705442809@qq.com>
2019-04-05 09:52:32 +0800

    no message

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by sjl on 2019/3/23.
+//  Copyright © 2019 sjl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+  
+        print(myString)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
